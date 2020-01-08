28f751c440d9aa1ab458f0c175d40577a9e4c45a
Vashist Srinivasan <vashist_nagesh@yahoo.co.in>
2020-01-08 17:35:10 +0530

    Corrected the public api list

--- projects/simple-animation/src/public-api.ts
+++ projects/simple-animation/src/public-api.ts
@@ -6,3 +6,4 @@ export * from './lib/simple-animation.service';
 export * from './lib/simple-animation.component';
 export * from './lib/simple-animation.module';
 export * from './lib/fade-in-out/fade-in-out.component';
+export * from './lib/shrinkable-header/shrinkable-header.directive';
